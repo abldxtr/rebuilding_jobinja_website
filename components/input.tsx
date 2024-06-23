24b6582c5e3d89e83cbfe4968e836ce4d3dafd6a
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-06-23 22:39:53 +0000

    new change

--- components/input.tsx
+++ components/input.tsx
@@ -84,147 +84,70 @@ export function Input1() {
 
 export function Input2() {
   const [open, setOpen] = useState(false);
-  const ref = useRef(null);
-  const Inputref = useRef(null);
-
-  const handleClickOutside = () => {
-    setOpen(false);
-  };
-
-  useOnClickOutside(ref, handleClickOutside);
-  useOnClickOutside(Inputref, handleClickOutside);
-  const [query, setQuery] = useState("");
-
-  const people = [
-    { id: 1, name: "Tom Cook" },
-    { id: 2, name: "Wade Cooper" },
-    { id: 3, name: "Tanya Fox" },
-    { id: 4, name: "Arlene Mccoy" },
-    { id: 5, name: "Devon Webb" },
-  ];
-  const [selected, setSelected] = useState(people[1]);
-
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) => {
-          return person.name.toLowerCase().includes(query.toLowerCase());
-        });
-
-  return (
-    <div
-      className=" mb-[10px] h-full w-full isolate "
-      // onClick={() => setOpen(!open)}
-      // ref={ref}
-    >
-      <ProvinceComboBox />
-    </div>
-  );
-}
-
-export function Input3() {
-  const frameworks = [
-    {
-      value: "next.js",
-      label: "Next.js",
-    },
-    {
-      value: "sveltekit",
-      label: "SvelteKit",
-    },
-    {
-      value: "nuxt.js",
-      label: "Nuxt.js",
-    },
-    {
-      value: "remix",
-      label: "Remix",
-    },
-    {
-      value: "astro",
-      label: "Astro",
-    },
-  ];
-  const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
   return (
-    // <div
-    //   className="relative mb-[10px] h-full w-full isolate "
-    //   // onClick={() => listener()}
-    //   onClick={() => setOpen(true)}
-    //   ref={ref}
-    // >
-    //   {/* <!-- icone --> */}
-
-    //   <div className="absolute right-[8px] top-[14px]">
-    //     <svg
-    //       xmlns="http://www.w3.org/2000/svg"
-    //       fill="none"
-    //       viewBox="0 0 24 24"
-    //       stroke-width="3"
-    //       stroke="currentColor"
-    //       className="size-[18px] cursor-pointer stroke-[#9d9d9d]"
-    //     >
-    //       <path
-    //         stroke-linecap="round"
-    //         stroke-linejoin="round"
-    //         d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-    //       ></path>
-    //     </svg>
-    //   </div>
-    //   {/* <!-- input --> */}
-    //   <input
-    //     type="text"
-    //     name="search"
-    //     id="search-01"
-    //     className={classNames(
-    //       "h-full w-full rounded-[3px] border border-[#e5e5e5] p-[10px] pr-[34px] text-[14px] outline-none ",
-    //       open ? "" : "[box-shadow:inset_0_-1px_0_1px_#d0d0d0]",
-    //     )}
-    //     placeholder="عنوان شغلی، نام شرکت، مهارت یا..."
-    //   />
-    //   {/* dropdown menue */}
-
-    //   <div
-    //     className={classNames(
-    //       " absolute w-full h-[120px] bg-white left-0 top-full mt-[-4px] px-[2px]  ",
-    //       open ? "  opacity-100" : " opacity-0 pointer-events-none ",
-    //     )}
-    //     ref={Inputref}
-    //   >
-    //     <div className=" px-[15px] pb-[10px]  ">
-    //       <input
-    //         type="text"
-    //         className="w-full rounded-[3px] border border-[#3ab1e4] p-[4px] mt-1 pr-[34px] text-[14px] outline-none focus:[box-shadow:inset_0_-1px_0_1px_#d0d0d0]  "
-    //       />
-    //     </div>
-    //   </div>
-    // </div>
-
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild className="w-full  ">
         <Button
           variant="Input"
           role="combobox"
           aria-expanded={open}
-          // className="w-full justify-between"
+          className="w-full justify-between text-[16px] flex items-center "
         >
-          {value
-            ? provinceOptions.find((item) => item.value === value)?.label
-            : "همه دسته بندی ها"}
+          <div className="flex items-center gap-2 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              // stroke="currentColor"
+              className=" fill-[#aaa] stroke-[#aaa] shrink-0 size-[18px] "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+
+            <div className=" text-[14px] flex mt-[1px]  text-[#aaa] ">
+              {value ? (
+                provinceOptions.find((item) => item.value === value)?.label
+              ) : (
+                <span className=" text-[14px] flex  text-[#aaa] ">
+                  همه استان‌ها
+                </span>
+              )}
+            </div>
+          </div>
+          <div>
+            <svg
+              height="20"
+              width="20"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+              focusable="false"
+              className={classNames(
+                " fill-[#aaa] shrink-0 transition-transform duration-200",
+                open ? "rotate-180" : "rotate-0",
+              )}
+            >
+              <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+            </svg>
+          </div>
           {/* <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" /> */}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="!w-[313px] p-0 !rounded-[0px] !max-h-[212px] !overflow-hidden ">
+      <PopoverContent className="!w-[313px] p-0 !rounded-[0px]  ">
         <Command>
           <CommandInput
-            placeholder=""
-            className="border border-[#3ab1e4] !rounded-[0px] !focus:outline-0 !p-[4px] !text-[12px] !px-[15px] !mb-[10px]   "
+            placeholder="جستجو..."
+            className="border border-[#3ab1e4] !rounded-[0px] !focus:outline-0 !p-[4px] !text-[12px] !px-[15px] !my-[10px] !h-[31px]   "
           />
           <CommandList>
             <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup className="!max-h-[212px] !overflow-hidden">
+            <CommandGroup className="!max-h-[212px] !overflow-y-auto ">
               {provinceOptions.map((framework) => (
                 <CommandItem
                   key={framework.value}
@@ -233,7 +156,100 @@ export function Input3() {
                     setValue(currentValue === value ? "" : currentValue);
                     setOpen(false);
                   }}
-                  className="border-b border-[#e5e5e5] text-[12px] px-[15px] cursor-poiner p-[6px] text-[#555] hover:bg-[#e5e5e5] font-normal "
+                  className="border-b border-[#e5e5e5] text-[14px] cursor-poiner px-[15px] cursor-poiner py-[6px] text-[#555] hover:bg-[#e5e5e5] font-normal "
+                >
+                  {/* <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0",
+                    )}
+                  /> */}
+                  {framework.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export function Input3() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild className="w-full  ">
+        <Button
+          variant="Input"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between text-[16px] flex items-center "
+        >
+          <div className="flex items-center gap-2 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className=" fill-[#aaa] stroke-[#aaa] shrink-0 size-[18px] "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+
+            <div className=" text-[14px] flex  text-[#aaa] ">
+              {value ? (
+                provinceOptions.find((item) => item.value === value)?.label
+              ) : (
+                <span className=" text-[14px] flex  text-[#aaa] ">
+                  همه دسته بندی ها
+                </span>
+              )}
+            </div>
+          </div>
+          <div>
+            <svg
+              height="20"
+              width="20"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+              focusable="false"
+              className={classNames(
+                " fill-[#aaa] shrink-0 transition-transform duration-200 ",
+                open ? "rotate-180" : "rotate-0",
+              )}
+            >
+              <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+            </svg>
+          </div>
+          {/* <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" /> */}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="!w-[313px] p-0 !rounded-[0px]  ">
+        <Command>
+          <CommandInput
+            placeholder="جستجو..."
+            className="border border-[#3ab1e4] !rounded-[0px] !focus:outline-0 !p-[4px] !text-[12px] !px-[15px] !my-[10px] !h-[31px]   "
+          />
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup className="!max-h-[212px] !overflow-y-auto ">
+              {provinceOptions.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                  className="border-b border-[#e5e5e5] text-[14px] cursor-poiner px-[15px] cursor-poiner py-[6px] text-[#555] hover:bg-[#e5e5e5] font-normal "
                 >
                   {/* <Check
                     className={cn(
